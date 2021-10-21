edff161eb5b67b73a7be700c7e0194ea37e7a5a1
Irene <irenesg1995@gmail.com>
2021-10-21 18:59:07 +0200

    finish slice

--- staff/irene-serrano/web/js/arrays/slice.js
+++ staff/irene-serrano/web/js/arrays/slice.js
@@ -1,8 +1,8 @@
-function slice(arr, start, end){
-    var result = [];
-   // debugger
-
-/*     end =
+function slice(arr, start, end) {
+  var result = [];
+  //debugger
+  start = start < 0 ? (arr.length + start < 0 ? 0 : arr.length + start) : start;
+  end =
     end === undefined
       ? arr.length
       : end > arr.length
@@ -10,31 +10,14 @@ function slice(arr, start, end){
       : end < 0
       ? arr.length + end
       : end;
-      */
 
-    end =
-        !end ? arr.length : (
-            end > arr.length ? arr.length : (
-                end < 0 ? end + arr.length : end
-            )
-
-        )
- 
-
-
-    /*     if(end){
-            if(end > arr.length){
-                end = arr.length
-            } else {
-                if(end < 0){
-                    if(end + arr.length < 0){ERRROR}
-                }
-            }
-        } */
-    for(var i = start; i < end; i++){
-        var element = arr[i];
-        result[result.length] = element;
+  for (var i = start; i < end; i++) {
+    if (end === 0) {
+      return result;
     }
-    
-    return result
+    var element = arr[i];
+    result[result.length] = element;
+  }
+
+  return result;
 }
